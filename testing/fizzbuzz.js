14137a6da07add0f9e23042966b641d355f8d1fa
AndriusIsin <isin.andrius@gmail.com>
2023-06-24 11:24:21 +0100

    ddd

--- testing/fizzbuzz.js
+++ testing/fizzbuzz.js
@@ -1,3 +1,17 @@
-function fizzbuzz(number) {}
+function fizzbuzz(number) {
+  let numberArr = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0) {
+      numberArr.push("fizz");
+    } else if (i % 5 === 0) {
+      numberArr.push("buzz");
+    } else if (i % 5 === 0 && i % 3 === 0) {
+      numberArr.push("fizzbuzz");
+    } else {
+      numberArr.push(i);
+    }
+  }
+  return numberArr.join(",");
+}
 
 export default fizzbuzz;
--- testing/fizzbuzz.test.js
+++ testing/fizzbuzz.test.js
@@ -8,3 +8,20 @@ test("it should return 1 when passed 1 as an argument", () => {
   // assert
   expect(result).toEqual("1");
 });
+test("it should return 1,2 when passed 2 as an argument", () => {
+  // arrange
+  let number = 2;
+  // act
+  let result = fizzbuzz(number);
+  // assert
+  expect(result).toEqual("1,2");
+});
+
+test("it should return 1,2,fizz when passed 3 as an argument", () => {
+  // arrange
+  let number = 3;
+  // act
+  let result = fizzbuzz(number);
+  // assert
+  expect(result).toEqual("1,2,fizz");
+});
